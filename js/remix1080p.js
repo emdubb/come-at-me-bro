66fe53676afbefe7015c6073b480613aca69f36f
Melissa Wilcox <melissa@emdubb.co>
2017-01-10 13:32:37 -0800

    export thumbnails for remix 1080p

--- js/remix1080p.js
+++ js/remix1080p.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
   var counter = 0;
   var canvasArray = [];
+  var malformedRatios = [];
   var zip = new JSZip();
   zip.folder("export");
-  $("#fileUpload").change(function(){
+  $("#remix1080FileUpload").change(function(){
     formatFiles(this, function(){
       exportFiles()
     });
   })
 
   function formatFiles(that, callback){
-    $("#downsample50-file-type-error").hide();
+    $("#remix1080error").text("")
     var files = that.files
     console.log(files)
     for (var i = 0; i < files.length; i++) {
@@ -20,13 +21,32 @@ $(document).ready(function(){
           var reader = new FileReader();
 
           reader.onload = function(event) {
-            var name = file.name
+            var error
+            var splitName = file.name.split('.');
+            var fileExtension = splitName.pop();
+            var name = splitName.join('.') + "_thumbnail." + fileExtension;
+
+            console.log("SPLIT: ", name)
             var url = event.target.result;
 
             var img = new Image();
             img.src = url;
-            img.width = img.width / 2
-            img.height = img.height / 2
+            var ratio = parseFloat((img.width/img.height).toFixed(5))
+
+            if (ratio == 0.66964) {
+              console.log("valid movie ratio");
+              img.width = 300
+              img.height = 448
+            } else if (ratio == 1) {
+              console.log("valid audio ratio");
+              img.width = 300
+              img.height = 300
+            } else {
+              console.log("invalid ratio", ratio)
+              malformedRatios.push(file.name);
+              console.log("MALFORMED: ", malformedRatios);
+              var error = true;
+            }
 
             var canvas = document.getElementById("canvas");
             canvas.width = img.width;
@@ -38,13 +58,19 @@ $(document).ready(function(){
               zip.file("export/" + name, blob);
               canvasArray.push(blob)
               if (canvasArray.length === files.length) {
+                if (error == true) {
+                  message = "<p>The following files were an improper ratio and were unable to be downsized: <em>" + malformedRatios.join(', '); + "</em></p>"
+                } else {
+                  message = ""
+                }
+                $("#remix1080error").append("<p>" + message + "</p>");
                 callback && callback();
               }
             })
 
           }
         } else {
-          $("#downsample50-file-type-error").show();
+          $("#remix1080error").append("<p>The uploaded file type is not supported! Only JPG, PNG, and SVG images are supported.</p>" );
         }
         reader.readAsDataURL(file);
       })(files[i]);
